1aecfc9ea9e755859f7b5d4727fd3e3190f7595b
Jonathan Hersh <jon@her.sh>
2014-09-27 17:45:55 -0700

    update linter

--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@
 SITE="s3://splinesoft.net/"
 
 bundle install
-bundle exec jekyll build --config _config.yml
 
-bundle exec htmlproof ./_site
+bundle exec jekyll build --config _config.yml
+bundle exec htmlproof --verbose --favicon ./_site
 
 find _site/ -iname '*.html' -exec gzip -n --best {} +
 find _site/ -iname '*.xml' -exec gzip -n --best {} +
